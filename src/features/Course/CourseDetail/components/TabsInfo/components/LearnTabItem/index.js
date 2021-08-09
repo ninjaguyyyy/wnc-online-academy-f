d7bc279dd1af815d60cc1c8c1830b5bbffad52d2
Torres Nguyen <torres.nguyen@spvn.local>
2021-08-09 23:56:33 +0700

    adding display video

--- src/features/Course/CourseDetail/components/TabsInfo/components/LearnTabItem/index.js
+++ src/features/Course/CourseDetail/components/TabsInfo/components/LearnTabItem/index.js
@@ -1,9 +1,27 @@
-import React from "react";
-import Accordion from "react-bootstrap/Accordion";
+import React, { useState, useRef } from "react";
+import { Accordion, Modal, Button } from "react-bootstrap";
 import { BsFileEarmarkText, BsPlay, BsLock, BsLockFill } from "react-icons/bs";
 import { toast } from "react-toastify";
+import ReactPlayer from "react-player";
+import "../../../../../../../../node_modules/video-react/dist/video-react.css";
+import { Player } from "video-react";
+import { ApiUrl } from "api/authUser";
 
 export default function LearnTabItem({ sections }) {
+  const [showModal, setShowModal] = useState(false);
+  const playerElement = useRef(null);
+
+  const handlePlayVideo = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    const { player } = playerElement.current.getState();
+    const lastWatchTime = player.currentTime;
+    console.log(lastWatchTime);
+  };
+
   return (
     <>
       {sections.length > 0 &&
@@ -25,7 +43,7 @@ export default function LearnTabItem({ sections }) {
                       </div>
                       <div>
                         {lecture.isPreview ? (
-                          <a href="javascript:void" style={{ color: "#656464" }}>
+                          <a href="javascript:void" onClick={handlePlayVideo} style={{ color: "#656464" }}>
                             <BsPlay size={22} />
                           </a>
                         ) : (
@@ -45,32 +63,27 @@ export default function LearnTabItem({ sections }) {
                     Lectures are being updated and coming soon!
                   </div>
                 )}
-                {/* {section.lectures.map((lecture, j) => (
-                            <Accordion key={lecture._id}>
-                              <Card>
-                                <Card.Body>
-                                  <Accordion.Toggle as={Button} eventKey={lecture._id}>
-                                    {lecture.title}
-                                  </Accordion.Toggle>
-                                </Card.Body>
-                                <Accordion.Collapse eventKey={lecture._id}>
-                                  <Card.Body>
-                                    <h2>{lecture.title}</h2>
-                                    <h2>Clip</h2>
-                                    <ReactPlayer
-                                      url="https://wnc-online-academy-21.herokuapp.com/resources/video/wnc21-wfcD6WlQ9e.mp4"
-                                      style={{ width: "600px" }}
-                                      controls={true}
-                                    />
-                                  </Card.Body>
-                                </Accordion.Collapse>
-                              </Card>
-                            </Accordion>
-                          ))} */}
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
         ))}
+      {/* dialogClassName="modal-90w" */}
+      <Modal show={showModal} onHide={() => handleCloseModal()} centered size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Payment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" /> */}
+          <Player ref={playerElement} startTime={20}>
+            <source src={`${"http://localhost:3001/"}resources/video/wnc21-NDZm7pXF4I.mp4`} type="video/mp4" />
+          </Player>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleCloseModal()}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
